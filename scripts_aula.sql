f0497c51d6703b8b04731ab4238fff4a7f5a417d
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2023-12-21 11:12:00 -0300

    mySql, lesson 26, max/min using inner join

--- scripts_aula.sql
+++ scripts_aula.sql
@@ -378,3 +378,20 @@ select c.s_nome_cliente, v.i_venda_venda, v.d_data_venda, v.f_valor_venda
 from venda v
 inner join cliente c on c.i_cliente_cliente = v.i_cliente_cliente
 order by f_valor_venda desc limit 3;
+
+/* aula 26 */
+
+select max(f_valor_venda) from venda;
+select min(f_valor_venda) from venda;
+
+select *,max(f_valor_venda) from venda;
+
+select * from venda;
+
+select * from venda
+where f_valor_venda = (select max(f_valor_venda) from venda);
+
+select c.s_nome_cliente, v.i_venda_venda, v.d_data_venda, v.f_valor_venda 
+from venda v
+inner join cliente c on c.i_cliente_cliente = v.i_cliente_cliente
+where v.f_valor_venda = (select min(f_valor_venda) from venda);
